a14faa74d28e863543b80f8b20ba68dd626f5318
Luis <6314335+luisfeliz79@users.noreply.github.com>
2024-02-09 22:30:18 -0500

    updates to self assign script

--- azure-monitor-agent-deployment/Scripted/self-assign.sh
+++ azure-monitor-agent-deployment/Scripted/self-assign.sh
@@ -1,4 +1,11 @@
+
 #!/bin/bash
+
+
+# This script allows each VM or VMSS Instance configure itself for monitoring
+# Ideally, we should have this script kick off an automation function to handle
+# the Extension and DCR modifications to avoid having to give it permissions
+# For now, it will handle it directly
 
 # Requires:
 #
@@ -12,7 +19,6 @@
 #    Done by the this script for VMSS instances
 
 # V1.0.0
-
 #Configure the User Assigned Managed Identity Resource ID
 msiId="/subscriptions/xxxxxx/resourcegroups/xxxxx/providers/microsoft.managedidentity/userassignedidentities/azure-monitor-enable"
 
@@ -25,8 +31,9 @@ regional_dces["eastus2"]="/subscriptions/xxxxx/resourceGroups/xxxxx/providers/Mi
 regional_dces["eastus"]="/subscriptions/xxxxx/resourceGroups/xxxxx/providers/Microsoft.Insights/dataCollectionEndpoints/eastusdce"
 regional_dces["centralus"]="/subscriptions/xxxxx/resourceGroups/xxxxx/providers/Microsoft.Insights/dataCollectionEndpoints/centralusdce"
 
-
-
+#Configure the AMA Version
+#See this https://learn.microsoft.com/en-us/azure/azure-monitor/agents/azure-monitor-agent-extension-versions
+amaVersion="1.28"
 
 # SCRIPT START
 echo "[$(date)] Azure Monitor Agent DCR/DCE Association Script"
@@ -59,22 +66,22 @@ python3Exists=$(whereis -b python3 | awk '{print $2}')
 pythonExists=$(whereis -b python | awk '{print $2}')
 
 if [[ "$jqExists" != "" ]]; then
-   echo "jq exists at $jqExists"
-   resourceId=$(echo $imdsMetadata | jq -r .resourceId)
-   VmName=$(echo $imdsMetadata | jq -r .name)
-   VmLocation=$(echo $imdsMetadata |jq -r .location)
+echo "jq exists at $jqExists"
+resourceId=$(echo $imdsMetadata | jq -r .resourceId)
+VmName=$(echo $imdsMetadata | jq -r .name)
+VmLocation=$(echo $imdsMetadata |jq -r .location)
 
 elif [[ "$python3Exists" != "" ]]; then
-   echo "python3 exists at $python3Exists"
-   resourceId=$(echo $imdsMetadata | python3 -c "import json, sys; print(json.loads(sys.stdin.read())['resourceId'])")
-   VmName=$(echo $imdsMetadata | python3 -c "import json, sys; print(json.loads(sys.stdin.read())['name'])")
-   VmLocation=$(echo $imdsMetadata | python3 -c "import json, sys; print(json.loads(sys.stdin.read())['location'])")
+echo "python3 exists at $python3Exists"
+resourceId=$(echo $imdsMetadata | python3 -c "import json, sys; print(json.loads(sys.stdin.read())['resourceId'])")
+VmName=$(echo $imdsMetadata | python3 -c "import json, sys; print(json.loads(sys.stdin.read())['name'])")
+VmLocation=$(echo $imdsMetadata | python3 -c "import json, sys; print(json.loads(sys.stdin.read())['location'])")
 
 elif [[ "$pythonExists" != "" ]]; then
-   echo "python exists at $pythonExists"
-   resourceId=$(echo $imdsMetadata | python -c "import json, sys; print(json.loads(sys.stdin.read())['resourceId'])")
-   VmName=$(echo $imdsMetadata | python -c "import json, sys; print(json.loads(sys.stdin.read())['name'])")
-   VmLocation=$(echo $imdsMetadata | python -c "import json, sys; print(json.loads(sys.stdin.read())['location'])")
+echo "python exists at $pythonExists"
+resourceId=$(echo $imdsMetadata | python -c "import json, sys; print(json.loads(sys.stdin.read())['resourceId'])")
+VmName=$(echo $imdsMetadata | python -c "import json, sys; print(json.loads(sys.stdin.read())['name'])")
+VmLocation=$(echo $imdsMetadata | python -c "import json, sys; print(json.loads(sys.stdin.read())['location'])")
 fi
 echo   "[$(date)] ResourceId: $resourceId"
 echo   "[$(date)] Name: $VmName"
@@ -99,46 +106,48 @@ done
 
 # Check to see if this a VMSS instance
 if [[ "${idArray[-4]}" = "virtualMachineScaleSets" ]]; then
-  echo "[$(date)] VMSS Instance detected";
-  isVmss=true;
-  
-  # Infer the VMSS resource Id (first 8 elements of the array)
-  vmId=$(for i in {0..7}; do echo -n "/${idArray[i]}"; done)
-  vmssName=${idArray[-3]}
-  vmssRg=${idArray[-7]}
+echo "[$(date)] VMSS Instance detected";
+isVmss=true;
 
-  # Install the Azure Monitor Extension
-  # This will happen for each instance individually
-  echo "[$(date)] Installing or Updating the Azure Monitor Agent Extension"
-  updateMeUrl="https://management.azure.com"$resourceId"/extensions/AzureMonitorLinuxAgent?api-version=2023-07-01"
-  vmExtensionPayload='{"properties":{"autoUpgradeMinorVersion":true,"enableAutomaticUpgrade":true,"publisher": "Microsoft.Azure.Monitor","settings": "{authentication:{managedIdentity:{identifier-name:'$msiName',identifier-value:'$msiId'}}}","type": "AzureMonitorLinuxAgent","typeHandlerVersion": "1.28"}}'
+# Infer the VMSS resource Id (first 8 elements of the array)
+vmId=$(for i in {0..7}; do echo -n "/${idArray[i]}"; done)
+vmssName=${idArray[-3]}
+vmssRg=${idArray[-7]}
 
-  echo $updateMeUrl
-  echo $vmExtensionPayload
-  az rest --method PUT --url "$updateMeUrl" --body "$vmExtensionPayload"
+# Install the Azure Monitor Extension for a virtual machine scale set instance
+# This will happen for each instance individually
+echo "[$(date)] Installing or Updating the Azure Monitor Agent Extension"
+updateMeUrl="https://management.azure.com"$resourceId"/extensions/AzureMonitorLinuxAgent?api-version=2023-07-01"
+#  vmExtensionPayload='{"properties":{"autoUpgradeMinorVersion":true,"enableAutomaticUpgrade":true,"publisher": "Microsoft.Azure.Monitor","settings": "{authentication:{managedIdentity:{identifier-name:'$msiName',identifier-value:'$msiId'}}}","type": "AzureMonitorLinuxAgent","typeHandlerVersion": "1.28"}}'
+vmExtensionPayload='{"location":"'$VmLocation'","properties":{"autoUpgradeMinorVersion":false,"enableAutomaticUpgrade":false,"publisher": "Microsoft.Azure.Monitor","settings": { "authentication":{"managedIdentity":{"identifier-name":"mi_res_id","identifier-value":"'$msiId'"}}},"type": "AzureMonitorLinuxAgent","typeHandlerVersion": "'$amaVersion'"}}'
+echo $updateMeUrl
+echo $vmExtensionPayload
 
-  if [[ "${idArray[-1]}" = "0" ]];  then
+az rest --method PUT -u "$updateMeUrl" --body "$vmExtensionPayload"
+
+if [[ "${idArray[-1]}" = "0" ]];  then
     echo "[$(date)] This is the first instance! thefore, will associate DCR/DCEs"
-  else
+else
     echo "[$(date)] This is not the first instance, therefore exiting at this point !"
     exit 0;
-  fi
+fi
 fi 
 
 # Check to see if this is a traditional VM
 if [[ ${idArray[-2]} = "virtualMachines" ]] && ! [[ $isVmss = true ]] ;
 then
-  echo "[$(date)] Virtual Machine detected";
-  vmId=$resourceId
+echo "[$(date)] Virtual Machine detected";
+vmId=$resourceId
 
-  # Install the Azure Monitor Extension
-  echo "[$(date)] Installing or Updating the Azure Monitor Agent Extension"
-  updateMeUrl="https://management.azure.com"$resourceId"/extensions/AzureMonitorLinuxAgent?api-version=2023-07-01"
-  vmExtensionPayload='{"location":"'$VmLocation'","properties":{"autoUpgradeMinorVersion":true,"enableAutomaticUpgrade":true,"publisher": "Microsoft.Azure.Monitor","settings": "{authentication:{managedIdentity:{identifier-name:'$msiName',identifier-value:'$msiId'}}}","type": "AzureMonitorLinuxAgent","typeHandlerVersion": "1.28"}}'
+# Install the Azure Monitor Extension for a virtual machine
+echo "[$(date)] Installing or Updating the Azure Monitor Agent Extension"
+updateMeUrl="https://management.azure.com"$resourceId"/extensions/AzureMonitorLinuxAgent?api-version=2023-07-01"
+vmExtensionPayload='{"location":"'$VmLocation'","properties":{"autoUpgradeMinorVersion":false,"enableAutomaticUpgrade":false,"publisher": "Microsoft.Azure.Monitor","settings": { "authentication":{"managedIdentity":{"identifier-name":"mi_res_id","identifier-value":"'$msiId'"}}},"type": "AzureMonitorLinuxAgent","typeHandlerVersion": "'$amaVersion'"}}'
 
-  echo $updateMeUrl
-  echo $vmExtensionPayload
-  az rest --method PUT --url "$updateMeUrl" --body "$vmExtensionPayload"
+
+echo $updateMeUrl
+echo $vmExtensionPayload
+az rest --method PUT -u "$updateMeUrl" --body "$vmExtensionPayload"
 
 
 
@@ -147,8 +156,8 @@ fi
 echo "[$(date)] Using this resourceId: $vmId"
 
 if [[ -z $vmId ]]; then
-  echo "[$(date)] Unable to determine VM Id"
-  #exit 1
+echo "[$(date)] Unable to determine VM Id"
+#exit 1
 fi
 
 # If we got here, we are either a regular VM or the first VMSS Instance
@@ -158,14 +167,14 @@ echo "[$(date)] Associating DCR"
 dcrAssociationName="syslogs-and-metrics-dcr"
 dcrAssociationUrl="https://management.azure.com"$vmId"/providers/Microsoft.Insights/dataCollectionRuleAssociations/"$dcrAssociationName"?api-version=2022-06-01"
 dcrPayload='{"properties":{"dataCollectionRuleId":"'$ruleId'"}}'
-az rest --method PUT --url "$dcrAssociationUrl" --body $dcrPayload
+az rest --method PUT -u "$dcrAssociationUrl" --body $dcrPayload
 
 echo "[$(date)] Associating DCE"
 #Associate the DCE
 dceAssociationName="configurationAccessEndpoint"
 dceAssociationUrl="https://management.azure.com"$vmId"/providers/Microsoft.Insights/dataCollectionRuleAssociations/"$dceAssociationName"?api-version=2022-06-01"
 dcePayload='{"properties":{"dataCollectionEndpointId":"'$EndpointId'"}}'
-az rest --method PUT --url "$dceAssociationUrl" --body $dcePayload
+az rest --method PUT -u "$dceAssociationUrl" --body $dcePayload
 
 echo "[$(date)] Done associating DCR/DCEs"
 
