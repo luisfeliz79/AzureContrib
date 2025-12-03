74a525faa0c0beffd6871071ca4fd5ccb9ab5668
Luis <6314335+luisfeliz79@users.noreply.github.com>
2025-12-03 13:49:49 -0500

    updates to alert-to-teams app

--- monitoring/alert-to-teams-sample/terraform/main.tf
+++ monitoring/alert-to-teams-sample/terraform/main.tf
@@ -1,20 +1,4 @@
 locals {
-  # resource_group_name         = "teams-alert-on-ase-rg"
-  # logic_app_name              = "teams-alert-on-ase"
-  # location                    = "eastus2"
-  # support_storageaccount_name = "lufelizlogap333"
-  # user_ip_address             = "162.226.7.217"
-  # support_loganalytics_name   = "lufelizlaw333"
-  # support_appinsights_name    = "lufelizappinsights333"
-  # support_keyvault_name       = "lufelizkv333"
-  # uami_name                   = "logicapp-uami333"
-  # subscription_id             = "f0df4358-9e4c-43aa-82ec-eebde4fd8233"
-
-  # tags = {
-  #   Environment = "Development"
-  #   Project     = "Teams Notifications Logic App"
-  # }
-
   resource_group_name         = "alert-to-teams-rg"
   logic_app_name              = "alert-to-teams-logic-app"
   location                    = "eastus2"
