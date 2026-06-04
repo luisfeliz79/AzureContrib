9e46e373fa903a95b56f3177a128ee6cb213eef7
Luis <6314335+luisfeliz79@users.noreply.github.com>
2026-06-03 23:56:03 -0400

    workbook upload

---
+++ compute/azure-automation-hybrid/terraform/locals.tf
@@ -0,0 +1,16 @@
+
+locals {
+  subscription_id         = "<subscription_id>"
+  resource_group_name     = "<resource-group-name>"
+  location                = "eastus2"
+  automation_account_name = "<automation-account-name>"
+  arc_gateway_name        = "<arc-gateway-name>"
+  storage_account_name    = "<storage-account-name>"
+  key_vault_name          = "<key-vault-name>"
+  law_name                = "<law-name>"
+  tags                   = {
+    environment = "poc"
+    project     = "hybrid-automation"
+  }
+
+}
