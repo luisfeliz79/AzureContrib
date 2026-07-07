61521c78eaf90b0bacb31134fa4da085ca53fdbc
Luis <6314335+luisfeliz79@users.noreply.github.com>
2026-07-07 13:34:47 -0400

    storage account tf sample

---
+++ storage/storage-account-cmk-all-services-sample/locals.tf
@@ -0,0 +1,8 @@
+locals {
+    subscription_id      = "<SUBID>"
+    key_vault_name       = "<KEY_VAULT_NAME>"
+    storage_account_name = "<STORAGE_ACCOUNT_NAME>"
+    container_name       = "<CONTAINER_NAME>"
+    resource_group_name  = "<RESOURCE_GROUP_NAME>"
+    location             = "<LOCATION>"
+}
